74ad8aed27007c3fc68d6f182f9f4c655cebaed4
Muchokibenson <128919897+Muchokibenson@users.noreply.github.com>
2023-07-17 14:37:01 +0300

    fix:soughted the authand login issue

---
+++ auth.php
@@ -0,0 +1,30 @@
+<?php
+$serverName ="localhost";
+$userName = "root";
+$password = "";
+$dbName = "user_registration";
+
+// create connection
+
+$conn = new mysqli($serverName,$userName,$password , $dbName);
+// check connection
+if($conn -> connect_error ){
+    die("Connection failed " .$conn->connect_error);
+
+}
+
+// retrieve dat from forms
+
+$username = $_POST['typeNameX'];
+
+$UpassWord = $_POST['typePassWordX'];
+
+//prepare SQ statement
+ $sql = "SELECT * FROM users WHERE userName = '$username' AND passcode = '$UpassWord'";
+
+
+
+
+ $conn->close();
+?>
+
